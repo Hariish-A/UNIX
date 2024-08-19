d0326e15c0d07f02112ab8669d47e44ff7c2fa6f
Hariish-A <hariishero@gmail.com>
2024-08-19 12:43:47 +0530

    Create q2.sh

---
+++ q2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Type of files Count"
+
+regular_files=$(find . -maxdepth 1 -type f | wc -l)
+echo "Regular files $regular_files"
+
+directories=$(find . -maxdepth 1 -type d | wc -l)
+echo "Directories $directories"
+
+soft_links=$(find . -maxdepth 1 -type l | wc -l)
+echo "Softlinks $soft_links"
